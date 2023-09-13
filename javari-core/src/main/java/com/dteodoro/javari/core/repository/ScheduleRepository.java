47c8967c0f2ea439ab6ed604ea7b913aa7d151b6
Dário <dario.teodoro@live.com>
2023-09-13 15:02:33 +0100

    Fix: Schedule bet order

--- javari-core/src/main/java/com/dteodoro/javari/core/repository/ScheduleRepository.java
+++ javari-core/src/main/java/com/dteodoro/javari/core/repository/ScheduleRepository.java
@@ -29,6 +29,6 @@ public interface ScheduleRepository extends JpaRepository<Schedule, UUID> {
 			" order by s.startDate ")
 	List<Schedule> findBySeasonIdAndWeekId(UUID seasonId,UUID weekId);
 
-	@Query("select s from Schedule s order by s.startDate")
+	@Query("select s from Schedule s order by s.startDate, s.status asc ")
 	List<Schedule> findAllOrderByStartDate();
 }
