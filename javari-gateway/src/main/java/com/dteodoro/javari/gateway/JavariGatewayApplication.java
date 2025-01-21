63eebec41ccebee66b36c12d85212088e3314079
Dário <dario.teodoro@neogrid.com>
2025-01-21 07:26:31 -0300

    update pom gateway

---
+++ javari-gateway/src/main/java/com/dteodoro/javari/gateway/JavariGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.dteodoro.javari.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavariGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavariGatewayApplication.class, args);
+    }
+
+}
